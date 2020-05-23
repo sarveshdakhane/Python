3d73a20c4fae08328015c5f1246501c7a0e20d5d
Sarvesh Dakhane <sarvesh.dakhane@hotmail.com>
2020-05-23 19:38:52 +0530

    New Changes

---
+++ HackerRank_Challenges/Pratice.py
@@ -0,0 +1,18 @@
+class student():
+    id:int
+    Student_Name:str
+    Student_Surname:str
+
+    def __init__(self,Name,Surname):
+        self.Name=Name
+        self.Surname=Surname
+
+    def fullname(self):
+         return self.Name + " " + self.Surname
+
+
+
+
+        
+     
+     
---
+++ HackerRank_Challenges/Programe.py
@@ -0,0 +1,6 @@
+from Pratice import *
+
+
+if __name__ == "__main__":
+    st=student("abc","xyz")
+    print(st.fullname())
