ff1a2736773befb00cbefc65b440131fcbd78af8
Sarvesh Dakhane <Sarvesh.Dakhane@hotmail.com>
2021-11-24 21:18:30 +0100

    Merge Short

--- Algo and DS in Python/MergeShort.py
+++ Algo and DS in Python/MergeShort.py
@@ -1,25 +1,13 @@
-# Python program for implementation of MergeSort
+
 def mergeSort(arr):
 	if len(arr) > 1:
-
-		# Finding the mid of the array
 		mid = len(arr)//2
-
-		# Dividing the array elements
 		L = arr[:mid]
-
-		# into 2 halves
 		R = arr[mid:]
 
-		# Sorting the first half
 		mergeSort(L)
-
-		# Sorting the second half
 		mergeSort(R)
-
 		i = j = k = 0
-
-		# Copy data to temp arrays L[] and R[]
 		while i < len(L) and j < len(R):
 			if L[i] < R[j]:
 				arr[k] = L[i]
@@ -29,7 +17,6 @@ def mergeSort(arr):
 				j += 1
 			k += 1
 
-		# Checking if any element was left
 		while i < len(L):
 			arr[k] = L[i]
 			i += 1
@@ -40,8 +27,6 @@ def mergeSort(arr):
 			j += 1
 			k += 1
 
-# Code to print the list
-
 
 def printList(arr):
 	for i in range(len(arr)):
@@ -49,7 +34,7 @@ def printList(arr):
 	print()
 
 
-# Driver Code
+
 if __name__ == '__main__':
 	arr = [12, 11, 13, 5, 6, 7]
 	print("Given array is", end="\n")
@@ -58,4 +43,4 @@ if __name__ == '__main__':
 	print("Sorted array is: ", end="\n")
 	printList(arr)
 
-# This code is contributed by Mayank Khanna
+
